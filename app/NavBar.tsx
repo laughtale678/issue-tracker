6d90aa7450fdf1a56f726ff2c082aa5f696ae025
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2024-02-19 21:15:27 -0800

    style link in nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,32 @@
+'use client';
 import Link from 'next/link';
 import { AiFillBug } from 'react-icons/ai';
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NavBar = () => {
-    const links = [
-        {label: 'Dashboard', href: '/'},
-        {label: 'Issues', href: '/issue'}
-    ]
+  const currentPath = usePathname();
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' }
+  ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug/></Link>
-        <ul className='flex space-x-6 '>
-            {links.map(link =>(<Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>))}
-        </ul>   
-    </nav>
+      <Link href="/"><AiFillBug /></Link>
+      <ul className='flex space-x-6 '>
+        {links.map(link =>
+          <Link
+            key={link.href}
+            className={
+              //third party library to conditionally apply classes
+              classnames({
+                'text-zinc-900': currentPath === link.href,
+                'text-zinc-500': currentPath !== link.href,
+                'hover:text-zinc-800 transition-colors': true
+              })}
+            href={link.href}> {link.label} </Link>)}
+      </ul>
+    </nav >
   )
 }
 
