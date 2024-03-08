bc39587bcaa5b1dc8337891bd55013abf3f6410d
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2024-03-07 16:25:53 -0800

    form error handle server side

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,37 +4,52 @@ import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from "axios";
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 interface IssueForm {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 
 
-function NewIssuePage() {
+const NewIssuePage = () => {
   const router = useRouter();
-  const {register, control, handleSubmit}  = useForm<IssueForm>();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState('');
+
   return (
-    <form
-    className='max-w-xl space-y-3'
-    onSubmit={handleSubmit(
-      async (data)=> {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-      })}>
+    <div className="max-w-xl">
+      {error && 
+      <Callout.Root color="red" className="mb-5">
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>}
+      <form
+        className='space-y-3'
+        onSubmit={handleSubmit(
+          async (data) => {
+            try {
+              await axios.post('/api/issues', data);
+              router.push('/issues');
+            } catch (error) {
+              setError('An error occurred while creating the issue.')
+            }
+          })}>
         <TextField.Root>
-            <TextField.Input placeholder='Title' {...register('title')}/>
+          <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
         <Controller
-         name="description"
+          name="description"
           control={control}
-          render={( { field }) =>  <SimpleMDE placeholder='Description' {...field}/>}
+          render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
         />
         <Button>Submit New Issue</Button>
-    </form>
+      </form>
+
+    </div>
+
   );
 }
 
